d29d83e0b6b4bc210fb8dd6328afbebb06c6eb19
PabloRochaRobles <pablorocharobles@uma.es>
2024-04-03 14:35:03 +0200

    Calendario

---
+++ proyecto-sii/src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: []
+})
+export class AppModule { }
