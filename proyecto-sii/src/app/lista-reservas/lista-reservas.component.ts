e634c1087ec4f8d64f407904789b119fdff1f629
Ruben Garcia <rgarcres2211@uma.es>
2024-04-08 18:33:14 +0200

    ListaReservas

--- proyecto-sii/src/app/lista-reservas/lista-reservas.component.ts
+++ proyecto-sii/src/app/lista-reservas/lista-reservas.component.ts
@@ -2,33 +2,51 @@ import { Component, Input, Pipe, PipeTransform } from '@angular/core';
 import { CommonModule, Time } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Reserva } from '../reserva';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-lista-reservas',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, FormsModule],
   templateUrl: './lista-reservas.component.html',
   styleUrl: './lista-reservas.component.css'
 })
 
 export class ListaReservasComponent{
 
-  reservas: Reserva[] = []
-  primeraHora: Time = {hours: 6, minutes: 0}
-  ultimaHora: Time = {hours: 23, minutes: 0}
+  //reservas: Reserva[] = []
+  horasDis: number[] = this.rellenarHoras();
+  horaSelec: string = ''
 
+  
+  rellenarHoras(): number[]{
+    let t: number[] = []
 
-  convertir(value: any): string {
-    if(!value) return '';
-    let t = {hours: value, minutes: 0};
-    const hora: string = t.hours < 10 ? '0' + t.hours : t.hours.toString();
-    const minutos: string = t.minutes < 10 ? t.minutes + '0' : t.minutes.toString();
+    for(let i=6; i<=23; i++){
+      t.push(i)
+    }
+    return t
+  }
 
-    return `${hora}:${minutos}`;
+  convertir(t: number): string {
+    if(!t) return '';
+    const hora: string = t < 10 ? '0' + t : t.toString();
+
+    return `${hora}:00`;
   }
   
   range(start: number, end: number): number[] {
     return Array(end - start + 1).fill(0).map((_, idx) => start + idx);
   }
 
+  hacerReserva(){
+    if(this.horaSelec == ''){
+      console.log('ERROR')
+    } else {   
+      let h = parseInt(this.horaSelec.substring(0,2))
+      console.log(h)
+      this.horasDis = this.horasDis.filter((elemento) => elemento != h)
+      this.horaSelec = ''
+    }
+  }
 }
