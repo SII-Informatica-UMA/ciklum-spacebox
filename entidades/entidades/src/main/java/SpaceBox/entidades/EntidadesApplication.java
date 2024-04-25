3ef03ee8ea954adc279282aacf027fa6a8186f09
Ruben Garcia <rgarcres2211@uma.es>
2024-04-25 17:33:44 +0200

    Modificacion

--- entidades/entidades/src/main/java/SpaceBox/entidades/EntidadesApplication.java
+++ entidades/entidades/src/main/java/SpaceBox/entidades/EntidadesApplication.java
@@ -1,25 +1,12 @@
 package SpaceBox.entidades;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import jakarta.persistence.EntityManagerFactory;
 
-@SpringBootApplication
-public class EntidadesApplication implements CommandLineRunner{
-
-	@Autowired
-	private EntityManagerFactory emf;
+public class EntidadesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EntidadesApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		
-	}
 
 }
