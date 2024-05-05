472946afc1bcd2ae60e33f337dfe77c8e1d352d5
PabloRochaRobles <pablorocharobles@uma.es>
2024-05-05 12:40:23 +0200

    DTOs

---
+++ entidades/entidades/src/main/java/SpaceBox/dtos/EventoDTO.java
@@ -0,0 +1,23 @@
+
+import loombok.* ;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode
+@ToString
+
+public class EventoDTO {
+    private Integer id ;
+    private String nombre ;
+    private String descripcion ;
+    private String observaciones ;
+    private String lugar ;
+    private int duracionMinutos ;
+    private String inicio ;
+    private String reglaRecurrencia ;
+    private Integer idCliente ;
+    private enum Tipo { DISPONIBILIDAD, CITA }
+    private Tipo tipo ;
+}
